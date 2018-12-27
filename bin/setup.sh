b94d16ede8c493d3b2377a25071e45d63bf6ed08
dave <dave@ip-192-168-1-153.ec2.internal>
2018-12-27 10:42:01 -0500

    installing plug

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,32 +1,39 @@
 #!/usr/bin/env bash
 
-function brewinstalls() {
+function brew_installs() {
 	brew install neovim
 	brew install fzy
 }
 
 read -p "Do you wish to install neovim and fzy (yn)? " yn
 case $yn in
-        [Yy]* ) brewinstalls;;
+        [Yy]* ) brew_installs;;
         [Nn]* ) echo -e 'coolio\n----------------';;
         * ) echo "Please answer yes or no.";;
 esac
 
-function configinstalls() {
+function config_installs() {
 	cp config/init.vim ~/.config/nvim/init.vim
 	cp config/nvimrc ~/.nvimrc
 }
 
 read -p "Do you wish to install configs (yn)? " yn
 case $yn in
-        [Yy]* ) configinstalls;;
+        [Yy]* ) config_installs;;
         [Nn]* ) echo -e 'coolio\n----------------';;
         * ) echo "Please answer yes or no.";;
 esac
 
+function plugin_installs() {
+	curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	
+	nvim +PlugInstall +qall
+}
+
 read -p "Do you wish to install plugins now (yn)? " yn
 case $yn in
-        [Yy]* ) nvim +PlugInstall +qall;;
+        [Yy]* ) plugin_installs;;
         [Nn]* ) echo 'coolio\n------------------';;
         * ) echo "Please answer yes or no.";;
 esac
